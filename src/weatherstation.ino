cdf3116a8f8912ec41bcfab53cc3d64161277a99
Joseph Grasser <jgrasser.jge@gmail.com>
2017-10-03 21:48:26 -0700

    Once over with VIM. Fix indentation issues

--- src/weatherstation.ino
+++ src/weatherstation.ino
@@ -18,15 +18,15 @@ DHT dht(DHTPIN, DHTTYPE);
 TCPClient client;
 
 void setup() {
-    RGB.control(true); 
-    RGB.color(0, 0, 0);
-    dht.begin();
+	RGB.control(true); 
+	RGB.color(0, 0, 0);
+	dht.begin();
 }
 
 void loop() {
-    delay(2000);
-    String oid = "";
-    int tstamp = Time.now();
+	delay(2000);
+	String oid = "";
+	int tstamp = Time.now();
 
 	// Reading temperature or humidity takes about 250 milliseconds!
 	// Sensor readings may also be up to 2 seconds 'old' (its a 
@@ -36,7 +36,7 @@ void loop() {
 	float t = dht.getTempCelcius();
 	// Read temperature as Farenheit
 	float f = dht.getTempFarenheit();
-  
+
 	// Check if any reads failed and exit early (to try again).
 	if (isnan(h) || isnan(t) || isnan(f)) {
 		Serial.println("Failed to read from DHT sensor!");
@@ -48,9 +48,9 @@ void loop() {
 	float hi = dht.getHeatIndex();
 	float dp = dht.getDewPoint();
 	float k = dht.getTempKelvin();
-    String oid_humidity = oid.format("sensor.%s.%s.humidity %f %d", "temp", "dht22", h, tstamp);
-    String oid_tempc = oid.format("sensor.%s.%s.tempc %f %d", "temp", "dht22", t, tstamp);
-    String oid_tempf = oid.format("sensor.%s.%s.tempf %f %d", "temp", "dht22", f, tstamp);
+	String oid_humidity = oid.format("sensor.%s.%s.humidity %f %d", "temp", "dht22", h, tstamp);
+	String oid_tempc = oid.format("sensor.%s.%s.tempc %f %d", "temp", "dht22", t, tstamp);
+	String oid_tempf = oid.format("sensor.%s.%s.tempf %f %d", "temp", "dht22", f, tstamp);
 	String oid_dewp = oid.format("sensor.%s.%s.dewp %f %d", "temp", "dht22", dp, tstamp);
 	String oid_heati = oid.format("sensor.%s.%s.heati %f %d", "temp", "dht22", hi, tstamp);
 
@@ -60,46 +60,45 @@ void loop() {
 	Serial.println( oid_dewp );
 	Serial.println( oid_heati );
 	
-    int tmp36read = analogRead(TMP36PIN); 
-    tstamp = Time.now();
-    // Read will be a value between 0 and 4096. This
-    // represents that fraction of the input voltage. 
-    // 0.8 mV per unit to be exact. 
+	int tmp36read = analogRead(TMP36PIN); 
+	tstamp = Time.now();
+	// Read will be a value between 0 and 4096. This
+	// represents that fraction of the input voltage. 
+	// 0.8 mV per unit to be exact. 
     
-    float voltage = tmp36read;
-    // voltage should be in mV units. 
-    // Centigrade temperature = [(analog voltage in mV) - 500] / 10
-    float temperature_in_celcius = (voltage - 500) / 20;
-    //T(°F) = T(°C) × 1.8 + 32
-    float temperature_in_fahrenheit = temperature_in_celcius * 1.8 + 32;
-    
-    String oid_tmp36_tempc = oid.format("sensor.%s.%s.tempc %f %d", "temp", "tmp36", temperature_in_celcius, tstamp);
-    String oid_tmp36_tempf = oid.format("sensor.%s.%s.tempf %f %d", "temp", "tmp36", temperature_in_fahrenheit, tstamp);
-    
-    Serial.println( oid_tmp36_tempc );
-    Serial.println( oid_tmp36_tempf );
-    
-    //read data from light sensor
-    int temt6000read = analogRead(TEMT6000PIN); 
-    tstamp = Time.now();
-    String oid_temt6000_read = oid.format("sensor.%s.%s.lightlevel %d %d", "lightlevel", "tmt6000", temt6000read, tstamp);
-    Serial.println( oid_temt6000_read );
-    
-    client.connect( GRAPHITE_HOSTNAME, GRAPHITE_PORT );
+	float voltage = tmp36read;
+	// voltage should be in mV units. 
+	// Centigrade temperature = [(analog voltage in mV) - 500] / 10
+	float temperature_in_celcius = (voltage - 500) / 20;
+	//T(°F) = T(°C) × 1.8 + 32
+	float temperature_in_fahrenheit = temperature_in_celcius * 1.8 + 32;
+   
+	String oid_tmp36_tempc = oid.format("sensor.%s.%s.tempc %f %d", "temp", "tmp36", temperature_in_celcius, tstamp);
+	String oid_tmp36_tempf = oid.format("sensor.%s.%s.tempf %f %d", "temp", "tmp36", temperature_in_fahrenheit, tstamp);
+   
+	Serial.println( oid_tmp36_tempc );
+	Serial.println( oid_tmp36_tempf );
+
+	//read data from light sensor
+	int temt6000read = analogRead(TEMT6000PIN); 
+	tstamp = Time.now();
+	String oid_temt6000_read = oid.format("sensor.%s.%s.lightlevel %d %d", "lightlevel", "tmt6000", temt6000read, tstamp);
+	Serial.println( oid_temt6000_read );
+
+	client.connect( GRAPHITE_HOSTNAME, GRAPHITE_PORT );
 	if( client.connected() ){
-	    Serial.println( "Writing over network" );
-	   	// Only send data if within acceptable range.
-	   	if (t > 0 && t < 100){
-	    	client.println(oid_humidity);
-	    	client.println(oid_tempc);
-	    	client.println(oid_tempf);
-	    	client.println(oid_dewp);
-	    	client.println(oid_heati);
-	    }
-	    client.println(oid_tmp36_tempc);
-	    client.println(oid_tmp36_tempf);
-	    
-	    client.println(oid_temt6000_read);
+		Serial.println( "Writing over network" );
+		// Only send data if within acceptable range.
+		if (t > 0 && t < 100){
+			client.println(oid_humidity);
+			client.println(oid_tempc);
+			client.println(oid_tempf);
+			client.println(oid_dewp);
+			client.println(oid_heati);
+		}
+		client.println(oid_tmp36_tempc);
+		client.println(oid_tmp36_tempf);
+		client.println(oid_temt6000_read);
 	}
 	client.flush();
 	client.stop();
