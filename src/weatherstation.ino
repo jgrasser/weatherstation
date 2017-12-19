962c347a0a6e558ac37d9e46b922279fc3011d50
vagrant <vagrant@precise64.(none)>
2017-12-19 06:34:31 +0000

    Revert "Cleaned up a lot, moved functionality to functions, etc"
    This reverts commit c46cb50a71342797a01739d42ab603cbe4d5e62a.

--- src/weatherstation.ino
+++ src/weatherstation.ino
@@ -1,90 +1,105 @@
+// This #include statement was automatically added by the Particle IDE.
 #include "Adafruit_DHT/Adafruit_DHT.h"
 
-#define LABEL_HUMIDITY       "HUMIDITY"
-#define LABEL_LIGHTLEVEL     "LIGHTLEVEL"
-#define LABEL_TEMPERATURE_C  "TEMPERATURE_C"
-#define LABEL_TEMPERATURE_F  "TEMPERATURE_F"
+#define TEMT6000PIN A2
+#define TMP36PIN A1
+#define DHTPIN D6
 
-#define PIN_DHT       D6
-#define PIN_TEMT6000  A2
-#define PIN_TMP36     A1
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11		// DHT 11 
+#define DHTTYPE DHT22		// DHT 22 (AM2302)
+//#define DHTTYPE DHT21		// DHT 21 (AM2301)
 
-#define SENSOR_DHT       "DHT22"
-#define SENSOR_TEMT6000  "TEMT6000"
-#define SENSOR_TMP36     "TMP36"
-
-#define DHTTYPE DHT22
-
-DHT dht(PIN_DHT, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);
 
 #define GRAPHITE_HOSTNAME "myserver"
 #define GRAPHITE_PORT 2003
 
-
 TCPClient client;
 
-float voltage_to_celcius(float voltage){
-        // voltage should be in mV units.
-        // Centigrade temperature = [(analog voltage in mV) - 500] / 10
-        float temperature_in_celcius = (voltage - 500) / 20;
-        return temperature_in_celcius;
-}
-
-float celcius_to_farenheit(float celcius){
-        //T(°F) = T(°C) ▒~W 1.8 + 32
-        float temperature_in_fahrenheit = celcius * 1.8 + 32;
-        return temperature_in_fahrenheit;
-}
-
-void log_it(char *sensor, char *label, float measurement){
-	int timestamp = Time.now();
-        String empty = "";
-        String metric = empty.format("sensor.%s.%s %f %d", label, sensor, measurement, timestamp);
-
-        client.connect( GRAPHITE_HOSTNAME, GRAPHITE_PORT );
-        if( client.connected() ){
-                Serial.println("Writing over network: " + metric);
-                client.println(metric);
-        }
-        client.flush();
-        client.stop();
-}
-
-void turn_off_indicator_light(){
-        RGB.control(true);
-        RGB.color(0, 0, 0);	
-}
- 
 void setup() {
-        turn_off_indicator_light();
+	RGB.control(true); 
+	RGB.color(0, 0, 0);
 	dht.begin();
 }
 
 void loop() {
 	delay(2000);
+	String oid = "";
+	int tstamp = Time.now();
 
-	float dht_humidity = dht.getHumidity();
-	float dht_temperature_c = dht.getTempCelcius();
-	float dht_temperature_f = dht.getTempFarenheit();
+	// Reading temperature or humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (its a 
+	// very slow sensor)
+	float h = dht.getHumidity();
+	// Read temperature as Celsius
+	float t = dht.getTempCelcius();
+	// Read temperature as Farenheit
+	float f = dht.getTempFarenheit();
 
 	// Check if any reads failed and exit early (to try again).
-        // Read is considered to fail if values are nan or not within expected range.
-	if (isnan(dht_humidity) || isnan(dht_temperature_c) || isnan(dht_temperature_f) || (dht_temperature_c > 0 && dht_temperature_c < 100)) {
+	if (isnan(h) || isnan(t) || isnan(f)) {
 		Serial.println("Failed to read from DHT sensor!");
 		return;
 	}
 
-        log_it(SENSOR_DHT, LABEL_HUMIDITY, dht_humidity);
-        log_it(SENSOR_DHT, LABEL_TEMPERATURE_F, dht_temperature_f);
-        log_it(SENSOR_DHT, LABEL_TEMPERATURE_C, dht_temperature_c);
+	// Compute heat index
+	// Must send in temp in Fahrenheit!
+	float hi = dht.getHeatIndex();
+	float dp = dht.getDewPoint();
+	float k = dht.getTempKelvin();
+	String oid_humidity = oid.format("sensor.%s.%s.humidity %f %d", "temp", "dht22", h, tstamp);
+	String oid_tempc = oid.format("sensor.%s.%s.tempc %f %d", "temp", "dht22", t, tstamp);
+	String oid_tempf = oid.format("sensor.%s.%s.tempf %f %d", "temp", "dht22", f, tstamp);
+	String oid_dewp = oid.format("sensor.%s.%s.dewp %f %d", "temp", "dht22", dp, tstamp);
+	String oid_heati = oid.format("sensor.%s.%s.heati %f %d", "temp", "dht22", hi, tstamp);
 
+	Serial.println( oid_humidity );
+	Serial.println( oid_tempc );
+	Serial.println( oid_tempf );
+	Serial.println( oid_dewp );
+	Serial.println( oid_heati );
 	
-	int tmp36read = analogRead(PIN_TMP36); 
-	float tmp36_temperature_c = voltage_to_celcius(tmp36read);
-	float tmp36_temperature_f = celcius_to_farenheit(tmp36_temperature_c);
-        log_it(SENSOR_TMP36, LABEL_TEMPERATURE_F, tmp36_temperature_c);   
-        log_it(SENSOR_TMP36, LABEL_TEMPERATURE_C, tmp36_temperature_f);   
+	int tmp36read = analogRead(TMP36PIN); 
+	tstamp = Time.now();
+	// Read will be a value between 0 and 4096. This
+	// represents that fraction of the input voltage. 
+	// 0.8 mV per unit to be exact. 
+    
+	float voltage = tmp36read;
+	// voltage should be in mV units. 
+	// Centigrade temperature = [(analog voltage in mV) - 500] / 10
+	float temperature_in_celcius = (voltage - 500) / 20;
+	//T(°F) = T(°C) × 1.8 + 32
+	float temperature_in_fahrenheit = temperature_in_celcius * 1.8 + 32;
+   
+	String oid_tmp36_tempc = oid.format("sensor.%s.%s.tempc %f %d", "temp", "tmp36", temperature_in_celcius, tstamp);
+	String oid_tmp36_tempf = oid.format("sensor.%s.%s.tempf %f %d", "temp", "tmp36", temperature_in_fahrenheit, tstamp);
+   
+	Serial.println( oid_tmp36_tempc );
+	Serial.println( oid_tmp36_tempf );
 
-	int temt6000read = analogRead(PIN_TEMT6000); 
-        log_it(SENSOR_TEMT6000, LABEL_LIGHTLEVEL, temt6000read);
+	//read data from light sensor
+	int temt6000read = analogRead(TEMT6000PIN); 
+	tstamp = Time.now();
+	String oid_temt6000_read = oid.format("sensor.%s.%s.lightlevel %d %d", "lightlevel", "tmt6000", temt6000read, tstamp);
+	Serial.println( oid_temt6000_read );
+
+	client.connect( GRAPHITE_HOSTNAME, GRAPHITE_PORT );
+	if( client.connected() ){
+		Serial.println( "Writing over network" );
+		// Only send data if within acceptable range.
+		if (t > 0 && t < 100){
+			client.println(oid_humidity);
+			client.println(oid_tempc);
+			client.println(oid_tempf);
+			client.println(oid_dewp);
+			client.println(oid_heati);
+		}
+		client.println(oid_tmp36_tempc);
+		client.println(oid_tmp36_tempf);
+		client.println(oid_temt6000_read);
+	}
+	client.flush();
+	client.stop();
 }
